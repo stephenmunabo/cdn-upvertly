4a583f1eafd64fbfec4696af694c4948a0e85ea3
Stephen Munabo <stephen.munabo@gmail.com>
2020-11-07 16:51:27 -0600

    update

--- upvertly/upvertly-lib.js
+++ upvertly/upvertly-lib.js
@@ -57,7 +57,7 @@
             $("<link/>", {
                 rel: "stylesheet",
                 type: "text/css",
-                href: "css/fun.css"
+                href: "http://cdn.upvertly.com/upvertly/upvertly-lib.css"
             }).appendTo("head");
 
             var f = function() {
